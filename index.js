be437ff2e40f3e0653129420681d870ccf53f950
Washma Tabassum <washmamalik2906@gmail.com>
2023-12-13 17:09:36 +0500

    vercel added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const passwordValidator = require('password-validator');
+
 const ecommerceRoutes = require('./ecommerceRoutes');
 const passwordStrengthRoutes = require('./passwordStrengthRoutes');
 

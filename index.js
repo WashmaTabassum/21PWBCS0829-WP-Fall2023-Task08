2c92ad5a5221e84f642091dd66b6b49213c1b66b
Washma Tabassum <washmamalik2906@gmail.com>
2023-12-13 13:15:55 +0500

    final

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const passwordValidator = require('password-validator');
+const ecommerceRoutes = require('./ecommerceRoutes');
+const passwordStrengthRoutes = require('./passwordStrengthRoutes');
+
+const PORT = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use('/ecommerce', ecommerceRoutes); // Mounting ecommerceRoutes
+app.use('/password', passwordStrengthRoutes); // Mounting passwordStrengthRoutes
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
